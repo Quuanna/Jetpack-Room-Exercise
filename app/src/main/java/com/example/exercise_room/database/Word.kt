c85545207acbe818da783073e3dea073d364c954
Anna <wenan@staff.1111.com.tw>
2024-06-28 15:11:14 +0800

    調整 Room @Entity 建立的data class名稱和宣告，以及primaryKey的預設值

--- app/src/main/java/com/example/exercise_room/database/Word.kt
+++ app/src/main/java/com/example/exercise_room/database/Word.kt
@@ -2,15 +2,19 @@ package com.example.exercise_room.database
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "word_book_table")
+/**
+ * 目前只搜尋特定欄位，提高索引效率
+ */
+@Entity(tableName = "word_book_table", indices = [Index(value = ["englishWord"])])
 data class Word(
-    @PrimaryKey(autoGenerate = true) val id: Int? = 0,
-    @ColumnInfo var enWord: String? = null,
-    @ColumnInfo val phoneticSymbol: String? = null,
-    @ColumnInfo val chWord: String? = null,
-    @ColumnInfo val type: String? = null,
-    @ColumnInfo val describe: String? = null,
-    @ColumnInfo val example: String? = null
+    @PrimaryKey(autoGenerate = true) val id: Int = 0, // Room 會自動為自動產生的主鍵處理空值
+    @ColumnInfo val englishWord: String= "",
+    @ColumnInfo val phoneticSymbol: String= "",
+    @ColumnInfo val chineseWord: String= "",
+    @ColumnInfo val wordType: String= "",
+    @ColumnInfo val describe: String= "",
+    @ColumnInfo val example: String= ""
 )
