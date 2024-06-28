d1c967f3036b2d3fe7ec98798751f4d3b3afa7b1
Anna <wenan@staff.1111.com.tw>
2024-06-28 13:32:12 +0800

    調整Repository為interface，降低耦合度和方便模擬測試

--- app/src/main/java/com/example/exercise_room/repo/WordRepository.kt
+++ app/src/main/java/com/example/exercise_room/repo/WordRepository.kt
@@ -1,35 +1,12 @@
 package com.example.exercise_room.repo
 
 import com.example.exercise_room.database.Word
-import com.example.exercise_room.database.WordDao
+import kotlinx.coroutines.flow.Flow
 
-
-class WordRepository(private val wordDao: WordDao) {
-
-    val allWord  = wordDao.getAll() // 排序
-//    val allWord  = wordDao.loadAllWords() // 不排序
-
-    // default Room runs suspend queries off the main thread
-    suspend fun insert(word: Word) {
-        wordDao.insert(word)
-    }
-
-    // 更新資料需要先撈取要改變的資料
-    suspend fun edit(oldWord: String, word: Word) {
-        val old = wordDao.findByData(oldWord)
-        old.word = word.word
-        wordDao.update(old)
-    }
-
-    // 刪除資料需要先撈取要刪除的資料
-    suspend fun delete(word: Word) {
-        val old = wordDao.findByData(word.word.toString())
-        wordDao.delete(old)
-    }
-
-    suspend fun deleteAll() {
-        wordDao.deleteAll()
-    }
-
-
+interface WordRepository {
+    val allWords: Flow<List<Word>>
+    suspend fun insert(word: Word)
+    suspend fun update(word: Word)
+    suspend fun delete(word: Word)
+    suspend fun deleteAll()
 }
