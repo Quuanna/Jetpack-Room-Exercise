0319ab4ca922db1eba1d1e515933c12a7dabb822
Anna <wenan@staff.1111.com.tw>
2024-07-01 14:22:18 +0800

    新增Room的相關測試，資料庫操作和DAO查詢驗證資料是否正確 1.驗證資料插入 2.驗證資料更新 3.驗證資料刪除資料 4.驗證資料刪除多筆資料 5.驗證Flow 查詢使用 Turbine library 來驗證 Flow emit的資料是否符合預期

--- app/src/androidTest/java/com/example/exercise_room/ItemDAOTest.kt
+++ app/src/androidTest/java/com/example/exercise_room/ItemDAOTest.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import app.cash.turbine.test
 import com.example.exercise_room.database.Word
 import com.example.exercise_room.database.WordBookDao
 import com.example.exercise_room.database.WordRoomDataBase
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Assert
@@ -33,26 +35,97 @@ class ItemDAOTest {
         dataBase.close()
     }
 
-    /**
-     * DAO Test
-     */
 
     // 驗證資料插入： 插入資料後，使用 DAO 查詢資料，驗證插入的資料是否正確。
     @Test
     fun testInsertItem() = runTest {
-        val data = Word(englishWord = "TEST") // 實際值
-        dao.insert(data)
+        val initialData = Word(id = 1, englishWord = "TEST") // 實際值
+        dao.insert(initialData)
 
         val expected = "TEST" // 預期
         // 驗證剛剛的資料
         val retriever = dao.getWordByEnWord(expected)
-        Assert.assertNotNull(retriever) // 結果不為null
-        Assert.assertEquals(expected, retriever?.englishWord) // 是否查到跟預期一樣
+        Assert.assertNotNull(retriever)
+        Assert.assertEquals(expected, retriever?.englishWord)
     }
 
-    // TODO  驗證資料更新: 更新資料後，使用 DAO 查詢資料，驗證更新後的資料是否正確。
+    // 驗證資料更新: 更新資料後，使用 DAO 查詢資料，驗證更新後的資料是否正確。
+    @Test
+    fun testUpdateItem() = runTest {
+        val initialData = Word(id = 1, englishWord = "TEST")
+        dao.insert(initialData)
 
-    // TODO　驗證資料刪除： 刪除資料後，使用 DAO 查詢資料，驗證資料是否已被刪除。
+        val expected = "UPDATED"
+        val updatedData = initialData.copy(englishWord = expected)
+        dao.update(updatedData)
 
-    // TODO　驗證 Flow 查詢： 使用 turbine 函式庫來驗證 Flow 發出的資料是否符合預期
+        val retrievedData = dao.getWordByEnWord(expected)
+        Assert.assertNotNull(retrievedData) // 結果不為null
+        Assert.assertEquals(expected, retrievedData?.englishWord)
+    }
+
+    // 驗證資料刪除： 刪除資料後，使用 DAO 查詢資料，驗證資料是否已被刪除。
+    @Test
+    fun testDeleteItem() = runTest {
+        val initialData = Word(id = 1, englishWord = "TEST")
+        dao.insert(initialData)
+
+        dao.delete(initialData)
+
+        val retrievedData = dao.getWordByEnWord("TEST")
+        Assert.assertNull(retrievedData)
+    }
+
+    // 驗證資料刪除: 刪除多筆資料，使用 DAO 查詢資料，驗證資料是否已被刪除。
+    @Test
+    fun testDeleteMultiItem() = runTest {
+        dao.insert(
+            Word(id = 1, englishWord = "TEST1"),
+            Word(id = 2, englishWord = "TEST2"),
+            Word(id = 3, englishWord = "TEST3"),
+            Word(id = 4, englishWord = "TEST4"),
+            Word(id = 5, englishWord = "TEST5")
+        )
+
+        dao.delete(
+            Word(id = 1, englishWord = "TEST1"),
+            Word(id = 2, englishWord = "TEST2")
+        )
+
+        val retrievedData1 = dao.getWordByEnWord("TEST1")
+        val retrievedData2 = dao.getWordByEnWord("TEST2")
+        Assert.assertNull(retrievedData1)
+        Assert.assertNull(retrievedData2)
+    }
+
+
+    // 驗證資料刪除: 刪除全部筆資料，使用 DAO 查詢資料，驗證資料是否已被刪除。
+    @Test
+    fun testAllDeleteDBItem() = runTest {
+        dao.insert(Word(englishWord = "TEST1"), Word(englishWord = "TEST2"))
+
+        dao.deleteAll()
+        val allData = dao.loadAllWords().first()
+        Assert.assertTrue(allData.isEmpty())
+    }
+
+    // 驗證 Flow 查詢： 使用 Turbine library 來驗證 Flow emit的資料是否符合預期
+    @Test
+    fun testFlowQueryDB()  = runTest {
+        dao.loadAllWords().test {
+            // 驗證空資料庫
+            Assert.assertEquals(emptyList<Word>(), awaitItem())
+            cancelAndIgnoreRemainingEvents()
+        }
+
+        val testData1 = Word(id= 1, englishWord = "TEST1")
+        val testData2 = Word(id= 2, englishWord = "TEST2")
+        dao.insert(testData1, testData2)
+
+        dao.loadAllWords().test {
+            // 驗證 Flow emit包含插入資料的列表，比對是否正確
+            Assert.assertEquals(listOf(testData1, testData2), awaitItem())
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
 }
